be43ee09b5fd8c097ed6421a40533c279d75a74d
jgchaparro <jgchaparro@outlook.com>
2024-01-06 11:27:39 +0100

    Changing settings .py

--- TsakonianDict/settings.py
+++ TsakonianDict/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('dictionary/static')),)
+STATIC_URL = '/dictionary/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
